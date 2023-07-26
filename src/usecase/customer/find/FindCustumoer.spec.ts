032735d0858f56094d8b52a4762ab81b58bb4916
Giovani Diccini <giovani.diccini@gmail.com>
2023-07-26 07:01:21 -0300

    implements find customer usecase

--- src/usecase/customer/find/FindCustumoer.spec.ts
+++ src/usecase/customer/find/FindCustumoer.spec.ts
@@ -3,6 +3,7 @@ import CustomerModel from '../../../infrastructure/customer/repository/sequelize
 import CustomerRepository from '../../../infrastructure/customer/repository/sequelize/customer.repository'
 import Customer from '../../../domain/customer/entity/customer'
 import Address from '../../../domain/customer/value-object/address'
+import FindCustomerUseCase from './FindCustomerUsecase'
 
 describe('Test find customer use case', () => {
 	let sequelize: Sequelize
@@ -27,11 +28,12 @@ describe('Test find customer use case', () => {
 		const customerRepository = new CustomerRepository()
 		const usecase = new FindCustomerUseCase(customerRepository)
 
-		const consumer = new Customer('123', 'John')
-		const address = new Address('Street', 123, 'city', 'zip')
-		consumer.changeAddress(address)
+		const customer = new Customer('123', 'John')
+		const address = new Address('Street', 123, 'Zip', 'city')
+		customer.changeAddress(address)
 
-		await customerRepository.create(consumer)
+		const created = await customerRepository.create(customer)
+		console.log({ created })
 
 		const input = { id: '123' }
 		const output = {
